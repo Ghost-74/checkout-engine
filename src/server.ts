16f003288546c0a1bd221e537c983207a6457e35
Ghost-74 <imkavin74@gmail.com>
2026-09-21 03:46:43 +0530

    Added post and get orders

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { supabase } from "./supabase.js";
 
 const app = express();
 
@@ -10,8 +11,64 @@ app.get("/", (req, res) => {
   });
 });
 
+//testing db
+app.get("/test-db", async (req, res) => {
+  const { data, error } = await supabase
+    .from("orders")
+    .select("*");
+
+  if (error) {
+    return res.status(500).json({
+      error: error.message
+    });
+  }
+
+  res.json(data);
+});
+
+//creating order
+app.post("/orders", async (req, res) => {
+  const { customerName, productName, quantity, totalAmount } = req.body;
+
+  const { data, error } = await supabase
+    .from("orders")
+    .insert([
+      {
+        customer_name: customerName,
+        product_name: productName,
+        quantity: quantity,
+        total_amount: totalAmount
+      }
+    ])
+    .select()
+    .single();
+
+  if (error) {
+    return res.status(500).json({
+      error: error.message
+    });
+  }
+
+  res.status(201).json(data);
+});
+
+//fetchin' orders
+app.get("/orders", async (req, res) => {
+  const { data, error } = await supabase
+    .from("orders")
+    .select("*");
+
+  if (error) {
+    return res.status(500).json({
+      error: error.message
+    });
+  }
+
+  res.json(data);
+});
+
 const PORT = 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
